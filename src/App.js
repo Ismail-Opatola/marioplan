b2932d2598b14fb5a5731a22d5ee579cb8b8d38b
Ismail Opatola <opatolamails@gmail.com>
2019-01-22 13:40:47 +0100

    add Dashbaord, ProjectList, ProjectSummray components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
+          <Switch>
+            <Route path='/' component={Dashboard}></Route>
+          </Switch>
         </div>
       </Router>
     );
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div>
+     <div className='card z-depth-0 project-summary'> 
+        <div className='card-content grey-text text-darken-3'>
+            <span className='card-title'>Project Title</span>
+            <p>Posted by the Net Ninja</p>
+            <p className='grey-text'>3rd Sept, 2am 2019</p>
+        </div>
+        </div> 
+    </div>
+  )
+}
+
+export default ProjectSummary
