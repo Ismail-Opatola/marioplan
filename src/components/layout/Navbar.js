d5001eb2c0056178f8dee3ee61731d3e96532268
Ismail Opatola <opatolamails@gmail.com>
2019-01-23 03:45:03 +0100

    add NavLinks, Icon and bg Image, CSS

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import mario from '../../mario.png'
+
 
 
 const Navbar = () => {
   return (
     <nav className='nav-wrapper grey darken-3'>
         <div className='container'>
-            <Link to='/' className='brand-logo'>MarioPlan</Link>
+            <Link to='/' className='brand-logo mario material-icons'><img src={mario} alt={'mario'} /></Link>
             <SignedInLinks />
             <SignedOutLinks />
         </div>
