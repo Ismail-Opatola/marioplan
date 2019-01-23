a2f572a5a25f75d1cd185b19dbb8303bdb3099f2
Ismail Opatola <opatolamails@gmail.com>
2019-01-23 13:13:09 +0100

    enable Authentication on firebase, add FirebaseReducer

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer'
 import projectReducer from './projectReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
     __auth: authReducer,
     __project: projectReducer,
-    __firestore: firestoreReducer
+    __firestore: firestoreReducer,
+    __firebase: firebaseReducer
 })
 
 export default rootReducer
