1bf20b222e91769e843fc3707e8f4de1865ee706
Ismail Opatola <opatolamails@gmail.com>
2019-01-25 16:15:50 +0100

    add post author id, fname  to each post

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,11 +4,14 @@ export const __createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore }) => {
         // make async call to db : add new project to firestore
         const firestore = getFirestore();
+        const profile = getState().__firebase.profile;
+        const authorId = getState().__firebase.auth.uid;
+
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: "Ryu",
-            authorLastName: "Yoshi",
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date()
         }).then(() => {
             dispatch({ type: 'CREATE_PROJECT', project: project })
