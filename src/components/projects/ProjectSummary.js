13565906f37a7c77b915eb2886d19fd5d56325de
Ismail Opatola <opatolamails@gmail.com>
2019-01-25 17:25:40 +0100

    add date formatting to projectSummary using firbase-timestamp and momentjs

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import  moment  from 'moment';
 
 const ProjectSummary = ({_project}) => {
+  // // const createdDate = () => {moment(_project.createdAt.toString()).calender()};
+  // console.log(_project.createdAt.toDate());
+  // console.log(_project);
+  // const createdDate = _project.createdAt.toDate();
+  // moment(createdDate)
   return (
     <div>
      <div className='card z-depth-0 project-summary'> 
         <div className='card-content grey-text text-darken-3'>
             <span className='card-title'>{_project.title}</span>
             <p>Posted by { _project.authorFirstName } { _project.authorLastName }</p>
-            <p className='grey-text'>3rd Sept, 2am 2019</p>
+            <p className='grey-text'>{moment(_project.createdAt.toDate()).calendar()}</p>
         </div>
         </div> 
     </div>
