d56fbf0bf46e3726a7ad4f2dada35ee94465fc30
Ismail Opatola <opatolamails@gmail.com>
2019-01-28 11:51:01 +0100

    add notification trigger for when someone sign up

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,59 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello, Zombies!");
+  response.send("Hello, Zombies!");
 });
 
 //create notifications collection in firestore
-const createNotification = (notification) => {
-    return admin.firestore().collection('notifications')
-        .add(notification)
-        .then((doc) => console.log('notification added', doc))
-}
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("notification added", doc));
+};
 
 // create a notification object, pass to notifications collection on creation of a new project
-exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate(doc => {
-  
-  const project = doc.data();
-  const notification = {
-      content: 'Added a new project',
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    const project = doc.data();
+    const notification = {
+      content: "Added a new project",
       user: `${project.authorFirstName} ${project.authorLastName}`,
       time: admin.firestore.FieldValue.serverTimestamp()
-  }
+    };
 
-  return createNotification(notification)
-  
-  // Next:
-  // To Deploy only functions and exclude dist :enter CLI
-  // $ firebase deploy --only functions
-  // go to firebase to see the new notifications collection added to storage, click on Functions-dahsboard..... go create a new project on site and check Firebase Functions-Logs to see if our func was evoked
-})
+    return createNotification(notification);
 
+    // Next:
+    // To Deploy only functions and exclude dist :enter CLI
+    // $ firebase deploy --only functions
+    // go to firebase to see the new notifications collection added to storage, click on Functions-dahsboard..... go create a new project on site and check Firebase Functions-Logs to see if our func was evoked
+  });
 
+// create new user notification usining the auth service
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin.firestore()
+    .collection("users")
+    .doc(user.uid) // using the user id from auth to ref the user data
+    .get() // retrieves the doc for that user , get() is async
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Joined the party",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
+    });
+});
 
 // Notes:
 
