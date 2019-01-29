80873cadf36875a60d5657ae6d1b2ec872a11457
Ismail Opatola <opatolamails@gmail.com>
2019-01-29 16:36:37 +0100

    yarn build and firebase deploy from CLI

--- src/App.js
+++ src/App.js
@@ -27,3 +27,9 @@ class App extends Component {
 }
 
 export default App;
+
+
+// headon to firebase console>>Hosting>>follow instructions ... firebase deploy
+// before we say $ firebase deploy we actually have to build our react app and put it into the /dist folder ... npm run build or yarn build
+// actually its put into a /build folder but since we have specified /dist folder as our deployment folder during $ firebase init, so let's move the build content into the /dist folder ... it would be the folder deployed automatically when we $ firebase deploy
+// $ firebase deploy
